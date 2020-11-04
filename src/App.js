12d2ca647936549a239dfaf922da209137580774
Lior Ben-David <liorbd@Liors-MacBook-Air.local>
2020-11-04 12:28:24 -0500

    Added Nav Menu

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
 import './App.css';
 import TextField from '@material-ui/core/TextField';
 import {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <Router>
+      <div className = "App">
+      <nav><ul> 
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/info">More Information</Link>
+          </li>
+        </ul></nav>
+        <Switch>
+            <Route path="/home">
+              <h1>Home</h1>
+              <TextField label = "My Zipcode"/>
+            </Route>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
+            <Route path="/info">
+              <h1>More Information</h1>
+            </Route>
+
+        </Switch>
       <header>
-        <TextField label = "My Zipcode"/>
+        
       </header>
     </div>
+    </Router>
   );
 }
 
