c23a8a72a77c7cdc14102ba19dbbed8077709008
Rudy Schneider <rks9pax@virginia.edu>
2020-11-14 14:18:06 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,18 @@ import {
 import {Home} from './Home.js'
 import {About} from './About.js'
 import {Info} from './Info.js'
+import ReactMapGL from 'react-mapbox-gl';
 
 function App() {
+
+  const [viewport, setViewport] = useState ({
+    width: 1200,
+    height: 500,
+    latitude: 38.034494,
+    longitude: -78.504003,
+    zoom: 13,
+  });
+
   return (
     <Router>
       <div className = "App">
@@ -28,7 +38,7 @@ function App() {
         </ul></nav>
         <Switch>
             <Route path="/about">
-              <h1>About Us</h1>
+              <h1>About</h1>
               <About/>
             </Route>
             <Route path="/info">
@@ -37,6 +47,11 @@ function App() {
             </Route>
             <Route path="/">
               <h1>Home</h1>
+              <ReactMapGL
+                mapboxApiAccessToken="process.env.pk.eyJ1IjoicnVkeXNjaG5laWRlciIsImEiOiJja2dwZWszazQwaXdjMnFwM3dodTk1ZHB4In0.yCwfcZYI9zIEaEO978lDqQ"
+                {...viewport}
+                onViewportChange={nextViewport => setViewport(nextViewport)}
+              />
               <Home/>
             </Route>
 
